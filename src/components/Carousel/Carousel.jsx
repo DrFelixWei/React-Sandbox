6733496f840d0edd3a8eb923dbc1840adf2fdfb6
Felix Wei <felixwei7@gmail.com>
2024-09-18 19:42:44 -0700

    added ability to double click to snap to carousel item

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useRef } from 'react';
-import './Carousel.css';
+import { useEffect, useRef } from 'react'
+import './Carousel.css'
 
 function Carousel({
   carouselItems = [
@@ -11,115 +11,124 @@ function Carousel({
     { id: 6, content: <div>6</div> },
   ],
 }) {
-  const containerRef = useRef(null);
-  const containerCarouselRef = useRef(null);
-  const carouselRef = useRef(null);
-  const isMouseDown = useRef(false);
-  const currentMousePos = useRef(0);
-  const lastMousePos = useRef(0);
-  const lastMoveTo = useRef(0);
-  const moveTo = useRef(0);
+  const containerRef = useRef(null)
+  const containerCarouselRef = useRef(null)
+  const carouselRef = useRef(null)
+  const isMouseDown = useRef(false)
+  const currentMousePos = useRef(0)
+  const lastMousePos = useRef(0)
+  const lastMoveTo = useRef(0)
+  const moveTo = useRef(0)
 
   const createCarousel = () => {
-    const carouselProps = onResize();
-    const carouselItems = carouselRef.current.querySelectorAll('.carousel-item');
-    const length = carouselItems.length;
-    const degress = 360 / length;
-    const gap = 20;
-    const tz = distanceZ(carouselProps.w, length, gap);
+    const carouselProps = onResize()
+    const carouselItems = carouselRef.current.querySelectorAll('.carousel-item')
+    const length = carouselItems.length
+    const degress = 360 / length
+    const gap = 20
+    const tz = distanceZ(carouselProps.w, length, gap)
 
-    const fov = calculateFov(carouselProps);
-    const height = calculateHeight(tz);
+    const fov = calculateFov(carouselProps)
+    const height = calculateHeight(tz)
 
-    containerRef.current.style.width = `${tz * 2 + gap * length}px`;
-    containerRef.current.style.height = `${height}px`;
+    containerRef.current.style.width = `${tz * 2 + gap * length}px`
+    containerRef.current.style.height = `${height}px`
 
     carouselItems.forEach((item, i) => {
-      const degressByItem = `${degress * i}deg`;
-      item.style.setProperty('--rotatey', degressByItem);
-      item.style.setProperty('--tz', `${tz}px`);
-    });
-  };
+      const degressByItem = `${degress * i}deg`
+      item.style.setProperty('--rotatey', degressByItem)
+      item.style.setProperty('--tz', `${tz}px`)
+    })
+  }
 
-  const lerp = (a, b, n) => n * (a - b) + b;
+  const lerp = (a, b, n) => n * (a - b) + b
 
   const distanceZ = (widthElement, length, gap) => {
-    return widthElement / 2 / Math.tan(Math.PI / length) + gap;
-  };
+    return widthElement / 2 / Math.tan(Math.PI / length) + gap
+  }
 
   const calculateHeight = (z) => {
-    const t = Math.atan((90 * Math.PI) / 180 / 2);
-    return t * 2 * z;
-  };
+    const t = Math.atan((90 * Math.PI) / 180 / 2)
+    return t * 2 * z
+  }
 
   const calculateFov = (carouselProps) => {
     const perspective = window
       .getComputedStyle(containerCarouselRef.current)
-      .perspective.split('px')[0];
+      .perspective.split('px')[0]
 
     const length =
       Math.sqrt(carouselProps.w * carouselProps.w) +
-      Math.sqrt(carouselProps.h * carouselProps.h);
-    return 2 * Math.atan(length / (2 * perspective)) * (180 / Math.PI);
-  };
+      Math.sqrt(carouselProps.h * carouselProps.h)
+    return 2 * Math.atan(length / (2 * perspective)) * (180 / Math.PI)
+  }
 
   const getPosX = (x) => {
-    currentMousePos.current = x;
-    moveTo.current = currentMousePos.current < lastMousePos.current ? moveTo.current - 2 : moveTo.current + 2;
-    lastMousePos.current = currentMousePos.current;
-  };
+    console.log("getPosX:", x)
+    currentMousePos.current = x
+    console.log(currentMousePos.current < lastMousePos.current ? moveTo.current - 2 : moveTo.current + 2)
+    moveTo.current = currentMousePos.current < lastMousePos.current ? moveTo.current - 2 : moveTo.current + 2
+    lastMousePos.current = currentMousePos.current
+  }
 
   const update = () => {
-    lastMoveTo.current = lerp(moveTo.current, lastMoveTo.current, 0.05);
-    carouselRef.current.style.setProperty('--rotatey', `${lastMoveTo.current}deg`);
-    requestAnimationFrame(update);
-  };
+    lastMoveTo.current = lerp(moveTo.current, lastMoveTo.current, 0.05)
+    carouselRef.current.style.setProperty('--rotatey', `${lastMoveTo.current}deg`)
+    requestAnimationFrame(update)
+  }
 
   const onResize = () => {
-    const boundingCarousel = containerCarouselRef.current.getBoundingClientRect();
+    const boundingCarousel = containerCarouselRef.current.getBoundingClientRect()
     return {
       w: boundingCarousel.width,
       h: boundingCarousel.height,
-    };
-  };
+    }
+  }
 
   const initEvents = () => {
-    const carousel = carouselRef.current;
+    const carousel = carouselRef.current
 
     carousel.addEventListener('mousedown', () => {
-      isMouseDown.current = true;
-      carousel.style.cursor = 'grabbing';
-    });
+      isMouseDown.current = true
+      carousel.style.cursor = 'grabbing'
+    })
     carousel.addEventListener('mouseup', () => {
-      isMouseDown.current = false;
-      carousel.style.cursor = 'grab';
-    });
-    containerRef.current.addEventListener('mouseleave', () => (isMouseDown.current = false));
+      isMouseDown.current = false
+      carousel.style.cursor = 'grab'
+    })
+    containerRef.current.addEventListener('mouseleave', () => (isMouseDown.current = false))
 
-    carousel.addEventListener('mousemove', (e) => isMouseDown.current && getPosX(e.clientX));
+    carousel.addEventListener('mousemove', (e) => isMouseDown.current && getPosX(e.clientX))
 
     carousel.addEventListener('touchstart', () => {
-      isMouseDown.current = true;
-      carousel.style.cursor = 'grabbing';
-    });
+      isMouseDown.current = true
+      carousel.style.cursor = 'grabbing'
+    })
     carousel.addEventListener('touchend', () => {
-      isMouseDown.current = false;
-      carousel.style.cursor = 'grab';
-    });
-    containerRef.current.addEventListener('touchmove', (e) => isMouseDown.current && getPosX(e.touches[0].clientX));
+      isMouseDown.current = false
+      carousel.style.cursor = 'grab'
+    })
+    containerRef.current.addEventListener('touchmove', (e) => isMouseDown.current && getPosX(e.touches[0].clientX))
 
-    window.addEventListener('resize', createCarousel);
+    window.addEventListener('resize', createCarousel)
 
-    update();
-    createCarousel();
-  };
+    update()
+    createCarousel()
+  }
 
   useEffect(() => {
-    initEvents();
+    initEvents()
     return () => {
-      window.removeEventListener('resize', createCarousel);
-    };
-  }, []);
+      window.removeEventListener('resize', createCarousel)
+    }
+  }, [])
+  
+
+  const handleCarouselItemDoubleClick = (id) => {
+    let newMoveTo = 360 / carouselItems.length * (carouselItems.length - id + 1)
+    moveTo.current = newMoveTo
+    lastMousePos.current = currentMousePos.current
+  }
 
   return (
     <>
@@ -131,7 +140,7 @@ function Carousel({
             <div className="carousel" ref={carouselRef}>
 
               {carouselItems.map((item) => (
-                <div key={item.id} className="carousel-item">
+                <div key={item.id} className="carousel-item" onDoubleClick={() => handleCarouselItemDoubleClick(item.id)}>
                   {item.content}
                 </div>
               ))}
@@ -142,7 +151,7 @@ function Carousel({
         </div>
       </div>
     </>
-  );
+  )
 }
 
-export default Carousel;
+export default Carousel
