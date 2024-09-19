3830d82f671114d3ae1e451857d7e6aedf3b9eee
Felix Wei <felixwei7@gmail.com>
2024-09-18 20:32:43 -0700

    seperated carousel into horizontal default and vertical versions

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,5 +1,8 @@
-import { useEffect, useRef } from 'react'
-import './Carousel.css'
+import { useState } from 'react'
+import HorizontalCarousel from './HorizontalCarousel'
+import VerticalCarousel from './VerticalCarousel'
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 
 function Carousel({
   carouselItems = [
@@ -10,172 +13,48 @@ function Carousel({
     { id: 5, content: <div>5</div> },
     { id: 6, content: <div>6</div> },
   ],
-  enableArrowKeyNavigation = false,
+  enableArrowKeyNavigation = true,
 }) {
-  const containerRef = useRef(null)
-  const containerCarouselRef = useRef(null)
-  const carouselRef = useRef(null)
-  const isMouseDown = useRef(false)
-  const currentMousePos = useRef(0)
-  const lastMousePos = useRef(0)
-  const lastMoveTo = useRef(0)
-  const moveTo = useRef(0)
-  const rotationSpeed = 2
 
-  const createCarousel = () => {
-    const carouselProps = onResize()
-    const carouselItems = carouselRef.current.querySelectorAll('.carousel-item')
-    const length = carouselItems.length
-    const degress = 360 / length
-    const gap = 20
-    const tz = distanceZ(carouselProps.w, length, gap)
-
-    const fov = calculateFov(carouselProps)
-    const height = calculateHeight(tz)
-
-    containerRef.current.style.width = `${tz * 2 + gap * length}px`
-    containerRef.current.style.height = `${height}px`
-
-    carouselItems.forEach((item, i) => {
-      const degressByItem = `${degress * i}deg`
-      item.style.setProperty('--rotatey', degressByItem)
-      item.style.setProperty('--tz', `${tz}px`)
-    })
-  }
-
-  const linearInterpolate = (a, b, n) => n * (a - b) + b
-
-  const distanceZ = (widthElement, length, gap) => {
-    return widthElement / 2 / Math.tan(Math.PI / length) + gap
-  }
-
-  const calculateHeight = (z) => {
-    const t = Math.atan((90 * Math.PI) / 180 / 2)
-    return t * 2 * z
-  }
-
-  const calculateFov = (carouselProps) => {
-    const perspective = window
-      .getComputedStyle(containerCarouselRef.current)
-      .perspective.split('px')[0]
-
-    const length =
-      Math.sqrt(carouselProps.w * carouselProps.w) +
-      Math.sqrt(carouselProps.h * carouselProps.h)
-    return 2 * Math.atan(length / (2 * perspective)) * (180 / Math.PI)
-  }
-
-  const getPosX = (x) => {
-    currentMousePos.current = x
-    moveTo.current = currentMousePos.current < lastMousePos.current ? moveTo.current - 2 : moveTo.current + 2
-
-    // Attempted to make carousel rotation bidirectional but it becomes too sensitive to mouse movement
-    // moveTo.current += currentMousePos.current - lastMousePos.current 
-
-    lastMousePos.current = currentMousePos.current
-  }
-
-  const update = () => {
-    lastMoveTo.current = linearInterpolate(moveTo.current, lastMoveTo.current, 0.05)
-    carouselRef.current.style.setProperty('--rotatey', `${lastMoveTo.current}deg`)
-    requestAnimationFrame(update)
-  }
-
-  const onResize = () => {
-    const boundingCarousel = containerCarouselRef.current.getBoundingClientRect()
-    return {
-      w: boundingCarousel.width,
-      h: boundingCarousel.height,
-    }
-  }
-
-  const initEvents = () => {
-    const carousel = carouselRef.current
-
-    carousel.addEventListener('mousedown', () => {
-      isMouseDown.current = true
-      carousel.style.cursor = 'grabbing'
-    })
-    carousel.addEventListener('mouseup', () => {
-      isMouseDown.current = false
-      carousel.style.cursor = 'grab'
-    })
-    containerRef.current.addEventListener('mouseleave', () => (isMouseDown.current = false))
-
-    carousel.addEventListener('mousemove', (e) => isMouseDown.current && getPosX(e.clientX))
-
-    carousel.addEventListener('touchstart', () => {
-      isMouseDown.current = true
-      carousel.style.cursor = 'grabbing'
-    })
-    carousel.addEventListener('touchend', () => {
-      isMouseDown.current = false
-      carousel.style.cursor = 'grab'
-    })
-    containerRef.current.addEventListener('touchmove', (e) => isMouseDown.current && getPosX(e.touches[0].clientX))
-
-
-    if (enableArrowKeyNavigation) {
-      window.addEventListener('keydown', handleKeyPress) // allow navigation with keyboard arrows
-    }
-
-    window.addEventListener('resize', createCarousel)
-
-    update()
-    createCarousel()
-  }
-
-  const handleKeyPress = (e) => {
-    switch (e.key) {
-      case 'ArrowLeft':
-        moveTo.current -= rotationSpeed
-        break
-      case 'ArrowRight':
-        moveTo.current += rotationSpeed
-        break
-      default:
-        break
-    }
-    // Update the carousel rotation immediately
-    carouselRef.current.style.setProperty('--rotatey', `${moveTo.current}deg`)
-  }
-
-
-  useEffect(() => {
-    initEvents()
-    return () => {
-      window.removeEventListener('resize', createCarousel)
-      window.removeEventListener('keydown', handleKeyPress)
-    }
-  }, [])
-  
-
-  const handleCarouselItemDoubleClick = (id) => {
-    let newMoveTo = 360 / carouselItems.length * (carouselItems.length - id + 1)
-    moveTo.current = newMoveTo
-    lastMousePos.current = currentMousePos.current
+  const [carouselType, setCarouselType] = useState('horizontal')
+  const handleToggleCarouselType = () => {
+    setCarouselType((prevType) => (prevType === 'horizontal' ? 'vertical' : 'horizontal'))
   }
 
   return (
     <>
       <div>Carousel</div>
-      <br />
-      <div className="content">
-        <div className="container" ref={containerRef}>
-          <div className="container-carousel" ref={containerCarouselRef}>
-            <div className="carousel" ref={carouselRef}>
+      <br/>
 
-              {carouselItems.map((item) => (
-                <div key={item.id} className="carousel-item" onDoubleClick={() => handleCarouselItemDoubleClick(item.id)}>
-                  {item.content}
-                </div>
-              ))}
-              
+      <ToggleButtonGroup
+        color="primary"
+        value={carouselType}
+        exclusive
+        onChange={handleToggleCarouselType}
+        aria-label="Platform"
+        sx={{backgroundColor: 'lightgray'}}
+      >
+        <ToggleButton value="horizontal">Horizontal</ToggleButton>
+        <ToggleButton value="vertical">Vertical</ToggleButton>
+      </ToggleButtonGroup>
+      
 
-            </div>
-          </div>
-        </div>
-      </div>
+      <br/>
+
+      {carouselType === 'horizontal' && (
+        <HorizontalCarousel
+          carouselItems={carouselItems}
+          enableArrowKeyNavigation={enableArrowKeyNavigation}
+        />
+      )}
+
+      {carouselType === 'vertical' && (
+        <VerticalCarousel
+          carouselItems={carouselItems}
+          enableArrowKeyNavigation={enableArrowKeyNavigation}
+        />
+      )}
+     
     </>
   )
 }
---
+++ src/components/Carousel/VerticalCarousel.jsx
@@ -0,0 +1,165 @@
+import { useEffect, useRef } from 'react'
+import './VerticalCarousel.css'
+
+function VerticalCarousel({
+  carouselItems,
+  enableArrowKeyNavigation,
+}) {
+  const containerRef = useRef(null)
+  const containerCarouselRef = useRef(null)
+  const carouselRef = useRef(null)
+  const isMouseDown = useRef(false)
+  const currentMousePos = useRef(0)
+  const lastMousePos = useRef(0)
+  const lastMoveTo = useRef(0)
+  const moveTo = useRef(0)
+  const rotationSpeed = 2
+
+  const createCarousel = () => {
+    const carouselProps = onResize()
+    const carouselItems = carouselRef.current.querySelectorAll('.vertical-carousel-item')
+    const length = carouselItems.length
+    const degrees = 360 / length
+    const gap = 20
+    const tz = distanceZ(carouselProps.h, length, gap)  // Use height for vertical
+
+    const fov = calculateFov(carouselProps)
+    const width = calculateWidth(tz)
+
+    containerRef.current.style.width = `${width}px`
+    containerRef.current.style.height = `${tz * 2 + gap * length}px`
+
+    carouselItems.forEach((item, i) => {
+      const degreesByItem = `${degrees * i}deg`
+      item.style.setProperty('--rotatex', degreesByItem)
+      item.style.setProperty('--tz', `${tz}px`)
+    })
+  }
+
+  const linearInterpolate = (a, b, n) => n * (a - b) + b
+
+  const distanceZ = (heightElement, length, gap) => {
+    return heightElement / 2 / Math.tan(Math.PI / length) + gap
+  }
+
+  const calculateWidth = (z) => {
+    const t = Math.atan((90 * Math.PI) / 180 / 2)
+    return t * 2 * z
+  }
+
+  const calculateFov = (carouselProps) => {
+    const perspective = window
+      .getComputedStyle(containerCarouselRef.current)
+      .perspective.split('px')[0]
+
+    const length =
+      Math.sqrt(carouselProps.w * carouselProps.w) +
+      Math.sqrt(carouselProps.h * carouselProps.h)
+    return 2 * Math.atan(length / (2 * perspective)) * (180 / Math.PI)
+  }
+
+  const getPosY = (y) => {
+    currentMousePos.current = y
+    moveTo.current = currentMousePos.current < lastMousePos.current ? moveTo.current - 2 : moveTo.current + 2
+
+    lastMousePos.current = currentMousePos.current
+  }
+
+  const update = () => {
+    lastMoveTo.current = linearInterpolate(moveTo.current, lastMoveTo.current, 0.05)
+    carouselRef.current.style.setProperty('--rotatex', `${lastMoveTo.current}deg`)
+    requestAnimationFrame(update)
+  }
+
+  const onResize = () => {
+    const boundingCarousel = containerCarouselRef.current.getBoundingClientRect()
+    return {
+      w: boundingCarousel.width,
+      h: boundingCarousel.height,
+    }
+  }
+
+  const initEvents = () => {
+    const carousel = carouselRef.current
+
+    carousel.addEventListener('mousedown', () => {
+      isMouseDown.current = true
+      carousel.style.cursor = 'grabbing'
+    })
+    carousel.addEventListener('mouseup', () => {
+      isMouseDown.current = false
+      carousel.style.cursor = 'grab'
+    })
+    containerRef.current.addEventListener('mouseleave', () => (isMouseDown.current = false))
+
+    carousel.addEventListener('mousemove', (e) => isMouseDown.current && getPosY(e.clientY))
+
+    carousel.addEventListener('touchstart', () => {
+      isMouseDown.current = true
+      carousel.style.cursor = 'grabbing'
+    })
+    carousel.addEventListener('touchend', () => {
+      isMouseDown.current = false
+      carousel.style.cursor = 'grab'
+    })
+    containerRef.current.addEventListener('touchmove', (e) => isMouseDown.current && getPosY(e.touches[0].clientY))
+
+    if (enableArrowKeyNavigation) {
+      window.addEventListener('keydown', handleKeyPress) // allow navigation with keyboard arrows
+    }
+
+    window.addEventListener('resize', createCarousel)
+
+    update()
+    createCarousel()
+  }
+
+  const handleKeyPress = (e) => {
+    switch (e.key) {
+      case 'ArrowUp':
+        moveTo.current += rotationSpeed
+        break
+      case 'ArrowDown':
+        moveTo.current -= rotationSpeed
+        break
+      default:
+        break
+    }
+    // Update the carousel rotation immediately
+    carouselRef.current.style.setProperty('--rotatex', `${moveTo.current}deg`)
+  }
+
+  useEffect(() => {
+    initEvents()
+    return () => {
+      window.removeEventListener('resize', createCarousel)
+      window.removeEventListener('keydown', handleKeyPress)
+    }
+  }, [])
+
+  const handleCarouselItemDoubleClick = (id) => {
+    let newMoveTo = 360 / carouselItems.length * (carouselItems.length - id + 1)
+    moveTo.current = newMoveTo
+    lastMousePos.current = currentMousePos.current
+  }
+
+  return (
+    <>
+      <div className="content">
+        <div className="container" ref={containerRef}>
+          <div className="container-vertical-carousel" ref={containerCarouselRef}>
+            <div className="vertical-carousel" ref={carouselRef}>
+              {carouselItems.map((item) => (
+                <div key={item.id} className="vertical-carousel-item" onDoubleClick={() => handleCarouselItemDoubleClick(item.id)}>
+                  {item.content}
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default VerticalCarousel
